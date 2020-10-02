1fa058fc8076ea8e68598418af2dfc283baed623
Bradley Plater <bradley.plater@bglgroup.co.uk>
2020-10-02 10:23:11 +0100

    created a mock repository implementing the interface

---
+++ RestApi/Data/MockRestApiRepo.cs
@@ -0,0 +1,27 @@
+﻿using RestApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApi.Data
+{
+	public class MockRestApiRepo : IRestApiRepo
+	{
+		public IEnumerable<Command> GetAllCommands()
+		{
+			var commands = new List<Command>
+			{
+				new Command { Id = 0, HowTo = "Boil an Egg", Line = "Boil water", Platform = "Kettle & pan" },
+				new Command { Id = 1, HowTo = "Cut Bread", Line = "Get a knife", Platform = "Knife and chopping board" },
+				new Command { Id = 2, HowTo = "Make cup of tea", Line = "Place tea bag in cup", Platform = "Kettle & cup" }
+			};
+			return commands;
+		}
+
+		public Command GetCommandById(int id)
+		{
+			return new Command { Id = 0, HowTo = "Boil an Egg", Line = "Boil water", Platform = "Kettle & pan" };
+		}
+	}
+}
