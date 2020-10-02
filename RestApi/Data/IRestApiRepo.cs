d5eb282bc417b5928f9f216deab6c4494975adbe
Bradley Plater <bradley.plater@bglgroup.co.uk>
2020-10-02 10:14:24 +0100

    Created a repo interface with a get all commands method

---
+++ RestApi/Data/IRestApiRepo.cs
@@ -0,0 +1,13 @@
+﻿using RestApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApi.Data
+{
+	public interface IRestApiRepo
+	{
+		IEnumerable<Command> GetAllCommands();
+	}
+}
