eb18f434597ba2e1aafbc930c95a5e20b18d7880
Bradley Plater <bradley.plater@bglgroup.co.uk>
2020-10-02 11:13:33 +0100

    created controller

---
+++ RestApi/Controllers/CommandsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestApi.Data;
+using RestApi.Model;
+using System.Collections.Generic;
+
+namespace RestApi.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockRestApiRepo _repository = new MockRestApiRepo();
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAllCommands();
+            return Ok(commandItems);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id )
+        {
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem);
+        }
+    }
+}
